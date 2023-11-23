a4307d4859f3b215fe3b025ca3810263fa33d3db
༺ᎷΛRƵIEL60D༻ <140960098+Marziel60D@users.noreply.github.com>
2023-11-23 21:15:16 +0800

    todays update!

---
+++ script.js
@@ -0,0 +1,28 @@
+function emailValidate(email) {
+  if(email.includes('@')) {
+    return true;
+  }
+  else {
+    return false;
+  }
+}
+
+
+function clickListener(event) {
+  e.preventDefault();
+  let emailInput = document.querySelector('#email');
+  let messageInput = document.querySelector('#message');
+
+  let emailText = emailInput.value;
+  let messageText = messageInput.value;
+
+  if(emailValidate(emailText) !== true) {
+    console.log('The email address must contain @');
+    return false;
+  }
+  console.log('Thanks for your message');
+}
+
+let submitButton = document.querySelector('#submit-button');
+
+submitButton.addEventListener('click', clickListener);
