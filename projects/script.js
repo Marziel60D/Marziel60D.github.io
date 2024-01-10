98b924866bcffabbb1e61eb26c60ca54e5dc0280
༺ᎷΛRƵIEL60D༻ <140960098+Marziel60D@users.noreply.github.com>
2024-01-10 16:15:18 +0800

    update!

---
+++ projects/script.js
@@ -0,0 +1,68 @@
+let term = ''
+const songContainer = document.getElementById('songs');
+
+const updateTerm = () => {
+    term = document.getElementById('searchInput').value;
+
+    if(!term || term === '') {
+        alert('Please enter a search term')
+    } else {
+
+        while(songContainer.firstChild) {
+            songContainer.removeChild(songContainer.firstChild);
+        }
+
+
+        const url =`https://itunes.apple.com/search?media=music&term=${term}`;
+        fetch(url)
+        .then( (response) => response.json() )
+        .then((data) => {
+        //  console.log(data.results);
+
+        const artists = data.results;
+            return artists.map(result => {
+                
+
+                const article = document.createElement('article'),
+                        artist = document.createElement('p'),
+                        song = document.createElement('p'),
+                        img = document.createElement('img'),
+                        audio = document.createElement('audio'),
+                        audioSource = document.createElement('source')
+                        // console.log(result);
+
+                        artist.innerHTML = result.artistName
+                        song.innerHTML = result.trackName
+                        img.src = result.artworkUrl100
+                        audioSource.src = result.previewUrl
+                        audio.setAttribute('controls', '')
+
+                        article.appendChild(img)
+                        article.appendChild(artist)
+                        article.appendChild(song)
+                        article.appendChild(audio)
+                        audio.appendChild(audioSource)
+
+                        songContainer.appendChild(article)
+
+                        
+
+            })
+        })
+        .catch(error => console.log('Request failed: ', error))
+
+    }
+}
+
+const searchBtn = document.querySelector('button')
+searchBtn.addEventListener('click', updateTerm)
+
+document.addEventListener('play', event => {
+    const audio = document.getElementsByTagName('audio');
+
+    for(let i = 0; i < audio.length; i++){
+        if(audio[i] != event.target){
+            audio[i].pause();
+        }
+    }
+}, true)
